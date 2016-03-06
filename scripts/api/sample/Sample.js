efa2e9e7f09ed35640cb8579379c524fd5075605
Scott Seixas <sseixas@steelbrick.com>
2016-03-06 14:58:01 -0800

    Changed serivce provider for sample and incremented version

--- scripts/api/sample/Sample.js
+++ scripts/api/sample/Sample.js
@@ -1,17 +1,18 @@
 module.exports = function(oauth2) {
 
+    var provider = 'SimpleServiceProvider';
     var client = require('../../client/Client.js')(oauth2);
 
     function read(uid, version) {
-        return client.read({reader: 'SampleServiceProvider', uid: uid, version: version});
+        return client.read({reader: provider, uid: uid, version: version});
     }
 
     function load(uid, context, version) {
-        return client.load({loader: 'SampleServiceProvider', uid: uid, context: context, version: version});
+        return client.load({loader: provider, uid: uid, context: context, version: version});
     }
 
     function save(model, version) {
-        return client.save({saver: 'SampleServiceProvider', model: model, version: version});
+        return client.save({saver: provider, model: model, version: version});
     }
 
     return {
