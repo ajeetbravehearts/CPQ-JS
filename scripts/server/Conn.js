7c8511a26beb2086c9b6300325f56dc5bc9e9ce4
Scott Seixas <sseixas@steelbrick.com>
2016-03-06 11:03:04 -0800

    Add refresh token callout to refresh credentials

--- scripts/server/Conn.js
+++ scripts/server/Conn.js
@@ -1,13 +1,47 @@
 var jsforce = require('jsforce');
+var request = require('request');
 
 module.exports = function(opts) {
 
+    var accessToken;
+
+    var rHeaders =  {
+        org_url: opts.url,
+        refresh_token: opts.refreshToken
+    };
+
+    var refresh = {
+        url: 'https://brick-rest-test.herokuapp.com/oauth/refresh',
+        headers: rHeaders
+    };
+
+    jsforce.OAuth2.prototype.refreshToken = function(refreshToken, callback) {
+        request(refresh, function (error, response, body) {
+            accessToken = parse(body).accessToken;
+            callback((!accessToken ? {message: error.message || error} : false), {access_token : accessToken, id: '' });
+        });
+    };
+
     function getConnection() {
-        return new jsforce.Connection({
-            instanceUrl: opts.url,
-            refreshToken: opts.refreshToken
+        return new Promise(function(resolve, reject) {
+            resolve(new jsforce.Connection({
+                instanceUrl: opts.url,
+                refreshToken: opts.refreshToken,
+                accessToken: accessToken,
+                clientId: true,
+                clientSecret: true
+            }));
         });
     }
+
+    function parse(obj) {
+        try {
+            return JSON.parse(obj);
+        } catch (e) {
+            return obj;
+        }
+    }
+
 
     return {
         getConnection: getConnection
