dce0b11c9ea5a341e94932f3c1cda6c563267f31
Scott Seixas <sseixas@steelbrick.com>
2016-03-06 19:53:33 -0800

    Added uglify to build with external source map

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 var browserify = require('browserify');
 var vinylSourceStream = require( 'vinyl-source-stream' );
+var sourcemaps = require('gulp-sourcemaps');
 var vinylBuffer = require( 'vinyl-buffer' );
 
 gulp.task('build', function() {
     return browserify('./scripts/main.js', {debug: true} ).bundle()
         .pipe( vinylSourceStream( 'cpq.js' ) )
         .pipe( vinylBuffer() )
+        .pipe( sourcemaps.init( {loadMaps: true} ) )
+        .pipe( uglify() )
+        .pipe( sourcemaps.write('./') )
         .pipe( gulp.dest( './build' ) );
 });
